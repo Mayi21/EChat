3b7da3ced310616517dcaaecd459ef01bd0e19bf
xaohii <xaohii@163.com>
2022-07-24 21:45:29 +0800

    增加了消息类型的枚举和用户实体类

---
+++ common/src/main/java/entity/UserInfo.java
@@ -0,0 +1,11 @@
+package entity;
+
+import lombok.Data;
+
+@Data
+public class UserInfo {
+	private String userName;
+	private String loginName;
+	private String passwd;
+
+}
---
+++ common/src/main/java/enums/MessageTypeEnum.java
@@ -0,0 +1,22 @@
+package enums;
+
+/**
+ * 消息类型的枚举类型：
+ * 通知：暂无用
+ * 单聊：一对一
+ * 群聊：一对多
+ * 广播：暂时无用
+ * */
+public enum MessageTypeEnum {
+	NOTIFICATION("notification"),
+	MESSAGE("message"),
+	SINGLE("single"),
+	GROUP("group"),
+	BROADCAST("broadcast");
+
+	private String value;
+
+	MessageTypeEnum(String value) {
+		this.value = value;
+	}
+}
