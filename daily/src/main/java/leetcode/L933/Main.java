b3d772ad63468ea5440a8ebd98dc908583a1687f
xaohii <xaohii@163.com>
2022-12-05 23:39:32 +0800

    better

--- daily/src/main/java/leetcode/L933/Main.java
+++ daily/src/main/java/leetcode/L933/Main.java
@@ -1,25 +1,37 @@
 package leetcode.L933;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 public class Main {
 	public static void main(String[] args) {
-
+		RecentCounter recentCounter = new RecentCounter();
+		System.out.println(recentCounter.ping(1));
+		System.out.println(recentCounter.ping(2));
+		System.out.println(recentCounter.ping(3));
+		System.out.println(recentCounter.ping(4));
+		System.out.println(recentCounter.ping(5));
+		System.out.println(recentCounter.ping(2999));
+		System.out.println(recentCounter.ping(3000));
+		System.out.println(recentCounter.ping(6001));
 	}
 }
 class RecentCounter {
-	List<Integer> list;
+	private Queue<Integer> list;
 
 	public RecentCounter() {
-		this.list = new ArrayList<>();
+		this.list = new ArrayDeque<>();
 	}
 
 	public int ping(int t) {
-		list = list.stream().filter(x -> x + 3000 >= t).collect(Collectors.toList());
+		if (list.isEmpty()) {
+			list.add(t);
+			return list.size();
+		}
+		while (!list.isEmpty() && list.peek() + 3000 < t) {
+			list.remove();
+		}
 		list.add(t);
-
 		return list.size();
 	}
 }
