dfb95476b54f8f883ba3a064374d48a9b30b280a
xaohii <xaohii@163.com>
2022-12-05 23:39:54 +0800

    update

--- daily/src/main/java/leetcode/L933/Main.java
+++ daily/src/main/java/leetcode/L933/Main.java
@@ -35,9 +35,3 @@ class RecentCounter {
 		return list.size();
 	}
 }
-
-/**
- * Your RecentCounter object will be instantiated and called as such:
- * RecentCounter obj = new RecentCounter();
- * int param_1 = obj.ping(t);
- */
